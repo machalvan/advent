afed2d47eac3a2a5d19819b8de8d0eafc1b37d3c
Marcus Hansson <machalvan@hotmail.com>
2019-11-03 17:30:28 +0100

    Setup flexboxgrid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import React from 'react';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { Col, Row} from 'react-styled-flexboxgrid'
+
+const theme = {
+  flexboxgrid: {
+    gridSize: 12, // columns
+    gutterWidth: 1, // rem
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Dog calendar
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>Dog calendar</div>
+      <Row>
+        <Col md={3}>1</Col>
+        <Col md={3}>2</Col>
+        <Col md={3}>3</Col>
+        <Col md={3}>4</Col>
+        <Col md={3}>5</Col>
+        <Col md={3}>6</Col>
+      </Row>
+      <Row>
+        <Col md={3}>1</Col>
+        <Col md={3}>2</Col>
+        <Col md={3}>3</Col>
+        <Col md={3}>4</Col>
+        <Col md={3}>5</Col>
+        <Col md={3}>6</Col>
+      </Row>
+      <Row>
+        <Col md={3}>1</Col>
+        <Col md={3}>2</Col>
+        <Col md={3}>3</Col>
+        <Col md={3}>4</Col>
+        <Col md={3}>5</Col>
+        <Col md={3}>6</Col>
+      </Row>
+      <Row>
+        <Col md={3}>1</Col>
+        <Col md={3}>2</Col>
+        <Col md={3}>3</Col>
+        <Col md={3}>4</Col>
+        <Col md={3}>5</Col>
+        <Col md={3}>6</Col>
+      </Row>
+    </ThemeProvider>
   );
 }
 
