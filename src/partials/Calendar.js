1a6e04045ec2efa010892471b9396a33a2f521a2
Marcus Hansson <machalvan@hotmail.com>
2019-11-03 18:24:13 +0100

    Add some styling

--- src/partials/Calendar.js
+++ src/partials/Calendar.js
@@ -3,8 +3,13 @@ import React from "react";
 
 const StyledCalendar = styled.div`
   max-width: 960px;
-  margin: 20px;
-  flex: 1 1 0;
+  margin: 20px auto; 
+`
+
+const StyledHeader = styled.h1`
+  text-align: center;
+  color: white;
+  font-family: Arial;
 `
 
 const StyledList = styled.ul`
@@ -14,26 +19,49 @@ const StyledList = styled.ul`
   max-width: 1000px;
   gap: 1em;
   list-style: none;
-  margin: 10px auto 0;
+  margin: 10px 20px 0;
   padding: 0;
 `
 
 const StyledListItem = styled.li`
   width: 100%;
-  border: 1px solid black;
+  border: 1px solid white;
+  color: white;
 `
+
+const windows = [
+  {id: 1},
+  {id: 2},
+  {id: 3},
+  {id: 4},
+  {id: 5},
+  {id: 6},
+  {id: 7},
+  {id: 8},
+  {id: 9},
+  {id: 10},
+  {id: 11},
+  {id: 12},
+  {id: 13},
+  {id: 14},
+  {id: 15},
+  {id: 16},
+  {id: 17},
+  {id: 18},
+  {id: 19},
+  {id: 20},
+  {id: 21},
+  {id: 22},
+  {id: 23},
+  {id: 24},
+]
 
 export const Calendar = () => {
   return (
     <StyledCalendar>
-      <div>Dog calendar</div>
+      <StyledHeader>Dog calendar</StyledHeader>
       <StyledList>
-        <StyledListItem>1</StyledListItem>
-        <StyledListItem>2</StyledListItem>
-        <StyledListItem>3</StyledListItem>
-        <StyledListItem>4</StyledListItem>
-        <StyledListItem>5</StyledListItem>
-        <StyledListItem>6</StyledListItem>
+        {windows.map(window => <StyledListItem>{window.id}</StyledListItem>)}
       </StyledList>
     </StyledCalendar>
   )
