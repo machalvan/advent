566af1f6200cc0832e6e3ffbf45c6677ea9646e4
Marcus Hansson <machalvan@hotmail.com>
2019-11-21 14:33:56 +0100

    Testing something

--- src/res/videos/dogs.js
+++ src/res/videos/dogs.js
@@ -1,13 +1,10 @@
-import dog1 from './dog1.mp4';
-import dog2 from './dog2.mp4';
-import dog3 from './dog3.mp4';
 import dog4 from './dog4.mp4';
 import dog24 from './dog24.mp4';
 
 export default {
-  dog1,
-  dog2,
-  dog3,
+  dog1: dog4,
+  dog2: dog4,
+  dog3: dog4,
   dog4,
   dog24,
 }
