c29a825eea30b5215f193f3318ea35d48074cebd
Marcus Hansson <machalvan@hotmail.com>
2019-11-29 15:06:26 +0100

    Rearrange the order

--- src/res/dogs.js
+++ src/res/dogs.js
@@ -2,7 +2,7 @@ import React from "react";
 import dog1 from './videos/dog1.mp4';
 import dog2 from './videos/dog2.mp4';
 import dog3 from './videos/dog3.mp4';
-import dog4 from './img/dog4.png';
+import dog4 from './img/image1.png';
 import dog5 from './videos/dog5.mp4';
 import dog6 from './videos/dog6.mp4';
 import dog7 from './videos/dog7.mp4';
@@ -17,10 +17,12 @@ import dog15 from './videos/dog15.mp4';
 import dog17 from './videos/dog17.mp4';
 import dog18 from './videos/dog18.mp4';
 import dog19 from './videos/dog19.mp4';
-import dog20 from './img/dog20.jpg';
+import dog20 from './img/image2.jpg';
 import dog21 from './videos/dog21.mp4';
 import dog22 from './videos/dog22.mp4';
 import dog23 from './videos/dog23.mp4';
+const dog16 = "Fyn580ZD91U"
+const dog24 = "0zYW6NXnDOU"
 
 export default [
   {type: 'mp4', src: dog1},
@@ -32,19 +34,19 @@ export default [
   {type: 'mp4', src: dog7},
   {type: 'mp4', src: dog8},
   {type: 'mp4', src: dog9},
+  {type: 'image', src: dog20},
   {type: 'mp4', src: dog10},
   {type: 'mp4', src: dog11},
   {type: 'mp4', src: dog12},
   {type: 'mp4', src: dog13},
   {type: 'mp4', src: dog14},
   {type: 'mp4', src: dog15},
-  {type: 'youtube', src: "Fyn580ZD91U"},
+  {type: 'youtube', src: dog16},
   {type: 'mp4', src: dog17},
   {type: 'mp4', src: dog18},
   {type: 'mp4', src: dog19},
-  {type: 'image', src: dog20},
   {type: 'mp4', src: dog21},
   {type: 'mp4', src: dog22},
   {type: 'mp4', src: dog23},
-  {type: 'youtube', src: "0zYW6NXnDOU"},
+  {type: 'youtube', src: dog24},
 ]
