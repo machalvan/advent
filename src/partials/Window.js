cd6d70af32b0964ff1f7dbe31255fca9ca43376a
Marcus Hansson <machalvan@hotmail.com>
2020-11-30 13:28:38 +0100

    Test

--- src/partials/Window.js
+++ src/partials/Window.js
@@ -54,6 +54,7 @@ const getAbleToOpen = (windowNumber) => {
   const currentDate = new Date()
   const december = 11
 
+  return true;
   return (
     currentDate.getMonth() === december &&
     currentDate.getDate() >= windowNumber
