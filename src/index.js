e1b4455ef2138329b54525f7b91315dc4e2c418b
Marcus Hansson <machalvan@hotmail.com>
2020-12-04 10:28:01 +0100

    Text adding alert for new update available

--- src/index.js
+++ src/index.js
@@ -9,4 +9,11 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onSuccess: () => console.log("SUCCESS"),
+  onUpdate: reg => {
+    alert('New version available! Ready to update?');
+    if (reg && reg.waiting) reg.waiting.postMessage({ type: 'SKIP_WAITING' });
+    window.location.reload();
+  },
+});
